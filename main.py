d69ec014cf0b985c7ad6da9785166a22114d8b0d
Kajonsak005 <64015016@kmitl.ac.th>
2023-09-07 14:36:49 +0700

    change condition

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 def revert_string(string):
+    if (len(string) < 1 or len(string) > 5000):
+        raise Exception("String over length or less")
     result = ""
     for word in string.split(" "):
         result += word[::-1]+" "
